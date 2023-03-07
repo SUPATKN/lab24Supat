7f8dd7bad8317699bfc329d62ddfe8815576835a
SUPATKN <yacht1400@gmail.com>
2023-03-07 23:17:18 +0700

    full

--- lab24.cpp
+++ lab24.cpp
@@ -24,11 +24,59 @@ ComplexNumber ComplexNumber::operator+(const ComplexNumber &c){
 	return ComplexNumber(real+c.real,imag+c.imag);
 }
 
+ComplexNumber operator+(double a,const ComplexNumber &c){
+	return ComplexNumber(a+c.real,c.imag);
+}
+
 ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 	return ComplexNumber(real-c.real,imag-c.imag);
 }
 
-//Write your code here
+ComplexNumber operator-(double a,const ComplexNumber &c){
+	return ComplexNumber(a-c.real,-c.imag);
+}
+
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber((real*c.real)-(imag*c.imag),(real*c.imag)+(imag*c.real));
+}
+
+ComplexNumber operator*(double a,const ComplexNumber &c){
+	return ComplexNumber(a*c.real,a*c.imag);
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	return ComplexNumber( ((real*c.real)+(imag*c.imag))/(pow(c.real,2)+pow(c.imag,2)) , ((imag*c.real)-(real*c.imag))/(pow(c.real,2)+pow(c.imag,2)));
+}
+
+ComplexNumber operator/(double a,const ComplexNumber &c){
+	return ComplexNumber((a*(c.real))/(pow(c.real,2)+pow(c.imag,2)) , (-1*a*c.imag)/(pow(c.real,2)+pow(c.imag,2)));
+}
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	if(real == c.real && imag == c.imag) return 1;
+	else return 0;
+}
+
+bool operator==(double a,const ComplexNumber &c){
+	if(a == c.real && 0 == c.imag) return 1;
+	else return 0;
+}
+
+double ComplexNumber::abs(){
+	return sqrt((pow(real,2)+pow(imag,2)));
+}
+
+double ComplexNumber::angle(){
+	return (atan2(imag,real))*180/M_PI;
+}
+
+ostream &operator<<(ostream &os, const ComplexNumber &c){
+	if(c.imag > 0 && c.real != 0) return os << c.real << "+" << c.imag << "i";
+	else if(c.imag < 0 && c.real != 0) return os << c.real << c.imag << "i";
+	else if(c.real == 0 && c.imag == 0) return os << "0";
+	else if(c.real == 0) return os << c.imag << "i";
+	else return os << c.real;
+}
 
 int main(){
 	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
